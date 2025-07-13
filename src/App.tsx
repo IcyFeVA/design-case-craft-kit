97fbef63e038f114a0682067cbcaf0ba1cfe38bc
IcyFeVA <zirn.pascal@gmail.com>
2025-07-12 21:55:21 -0700

    feat: Add DefaultCaseStudy component with detailed project overview and navigation

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Index from "./pages/Index";
+import Index from "./pages/DefaultCaseStudy";
 import UIGallery from "./pages/UIGallery";
 import MindSpaceCaseStudy from "./pages/MindSpaceCaseStudy";
 import EcommerceCaseStudy from "./pages/EcommerceCaseStudy";
