602a07163f930e91915be89eb2d6e0d5daa67513
IcyFeVA <zirn.pascal@gmail.com>
2025-09-02 18:07:39 -0700

    logo in footer when dark mode

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,12 +1,18 @@
 import { Link } from "react-router-dom";
 import { Palette, Video } from "lucide-react";
+import { useTheme } from "next-themes";
 
 const Footer = () => {
+  const { resolvedTheme } = useTheme();
+
   return (
-    <footer className="border-t border-border py-8">
+    <footer className="border-t border-border py-4">
       <div className="max-w-6xl mx-auto px-6">
         <div className="flex flex-col md:flex-row justify-between items-center space-y-4 md:space-y-0">
-          <p className="text-muted-foreground">© 2025 Pascal Zirn. All rights reserved.</p>
+          <div className="flex items-center">
+            {resolvedTheme === 'dark' && <img src="/img/logo.png" alt="Logo" className="mr-2 mix-blend-screen" />}
+            <p className="text-muted-foreground">© 2025 Pascal Zirn. All rights reserved.</p>
+          </div>
           <div className="flex items-center space-x-6">
             <Link to="https://artstation.com/icyfeva" target="_blank" className="text-muted-foreground hover:text-primary transition-colors">
               <Palette className="w-5 h-5" /><span className="text-muted-foreground text-sm">Artstation</span>
