e1fa9c5324b4154f51089007327239b928e90d32
IcyFeVA <zirn.pascal@gmail.com>
2025-12-26 09:12:38 -0800

    new me

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -776,7 +776,7 @@ const Home = () => {
             <div className="space-y-6">
               <div className="aspect-square bg-gradient-to-br from-gray-200 to-gray-300 rounded-2xl flex items-center justify-center">
                 <div className="text-center space-y-2">
-                  <img src="/img/meagain.png" alt="Pascal Zirn" className="rounded-2xl" />
+                  <img src="/img/meagainonew.png" alt="Pascal Zirn" className="rounded-2xl" />
                 </div>
               </div>
 
